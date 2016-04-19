c96c837a72789c0551515ccddd30e1e38d886fc5
Stefan Uzunov <stefan.uzunov@scalefocus.com>
2016-04-19 18:41:40 +0300

    Added fetch globals and settings.

---
+++ src/public/javascript/http/fetchGlobals.js
@@ -0,0 +1,15 @@
+/**
+ * Created by stefan.uzunov on 4/19/2016.
+ */
+
+module.exports = {
+    checkStatus:(response) => {
+        if (response.status >= 200 && response.status < 300) {
+            return response
+        } else {
+            var error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+        }
+    }
+};
---
+++ src/public/javascript/http/fetchSettings.js
@@ -0,0 +1,15 @@
+/**
+ * Created by stefan.uzunov on 4/19/2016.
+ */
+
+
+
+module.exports = {
+    postRequest:{
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+};
